3f2ce8bedd8d639028e5a13df3e1c5f60cc29afb
Richard Mustkivi <richard.mustkivi@gmail.com>
2025-05-10 16:05:04 +0300

    Basic movement

---
+++ test game/Assets/Scripts/InputManager.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    private PlayerInput playerInput;
+    private PlayerInput.OnFootActions onFoot;
+    
+    private PlayerMotor motor;
+    private PlayerLook look;
+    void Awake()
+    {
+        playerInput = new PlayerInput();
+        onFoot = playerInput.OnFoot;
+        motor = GetComponent<PlayerMotor>();
+        look = GetComponent<PlayerLook>();
+        onFoot.Jump.performed += ctx => motor.Jump();
+    }
+    
+    void FixedUpdate()
+    {
+        motor.ProcessMove(onFoot.Movement.ReadValue<Vector2>());
+    }
+    void LateUpdate()
+    {
+        look.ProcessLook(onFoot.Look.ReadValue<Vector2>());
+    }
+    private void OnEnable()
+    {
+        onFoot.Enable();
+    }
+
+    private void OnDisable()
+    {
+        onFoot.Disable();
+    }
+}
---
+++ test game/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool IsGrounded;
+    public float speed = 5f;
+    public float gravity = -9.8f;
+    public float jumpHeight = 3f;
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+
+    }
+    public void ProcessMove(Vector2 input)
+    {
+        Vector3 moveDirection = Vector3.zero;
+        moveDirection.x = input.x;
+        moveDirection.z = input.y;
+        controller.Move(transform.TransformDirection(moveDirection) * speed * Time.deltaTime);
+        playerVelocity.y += gravity * Time.deltaTime;
+        if(IsGrounded && playerVelocity.y < 0)
+            playerVelocity.y = -2f;
+        controller.Move(playerVelocity * Time.deltaTime);
+        Debug.Log(playerVelocity.y);
+    }
+    public void Jump()
+    {
+        if (IsGrounded)
+        {
+            playerVelocity.y = Mathf.Sqrt(jumpHeight * -3.0f * gravity);
+        }
+    }
+    void Update()
+    {
+        IsGrounded = controller.isGrounded;
+    }
+}
